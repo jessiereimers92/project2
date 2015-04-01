6a589d9e0e904574428e1de978b0ef5ebd5e2c79
jessiereimers92 <jessiereimers92@gmail.com>
2015-04-01 00:38:39 -0400

    part 2
    part2

---
+++ part2/demo/js/part2.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+
+  // This line calls the "sayHello" function.
+  console.log('jQuery is running');
+
+
+$(function(){
+      SyntaxHighlighter.all();
+    });
+    $(window).load(function(){
+      $('#carousel').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        itemWidth: 210,
+        itemMargin: 5,
+        asNavFor: '#slider'
+      });
+
+      $('#slider').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        sync: "#carousel",
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+      });
+    });
+});
